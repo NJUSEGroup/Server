083e26ec59e0b38a33ab695ec222070ccfcb9a0f
yaozheng1998 <1150681747@qq.com>
2016-10-16 17:35:46 +0800

    10/16 姚政 提交CreditRecordServiceStub中依赖UserService的部分

--- HotelReservationSystem-parent/HotelReservationSystem-StubAndDriver/src/test/java/hrs/StubAndDriver/Service/CreditRecordService/CreditRecordServiceStub.java
+++ HotelReservationSystem-parent/HotelReservationSystem-StubAndDriver/src/test/java/hrs/StubAndDriver/Service/CreditRecordService/CreditRecordServiceStub.java
@@ -6,16 +6,24 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 
 import hrs.StubAndDriver.DAO.CreditRecordDAO.CreditRecordDAOStub;
+import hrs.StubAndDriver.Service.UserService.UserServiceStub;
 import hrs.client.Service.CreditRecordService;
+import hrs.client.Service.UserService;
 import hrs.client.VO.CreditRecordVO;
 import hrs.client.VO.HotelDiscountVO;
+import hrs.client.VO.UserVO;
 import hrs.common.DAO.CreditRecordDAO;
 import hrs.common.PO.CreditRecordPO;
 import hrs.common.PO.HotelDiscountPO;
 import hrs.common.util.ResultMessage;
 
 public class CreditRecordServiceStub implements CreditRecordService {
-	private CreditRecordDAO creditRecordDAO=new CreditRecordDAOStub();
+	private CreditRecordDAO creditRecordDAO;
+	private UserService  userService;
+	public CreditRecordServiceStub() {
+		creditRecordDAO=new CreditRecordDAOStub();
+		userService=new UserServiceStub();
+	}
 
 	@Override
 	public List<CreditRecordVO> findByUsername(String username) {
@@ -29,6 +37,8 @@ public class CreditRecordServiceStub implements CreditRecordService {
 	public ResultMessage add(CreditRecordVO creditrecordvo) {
 		CreditRecordPO po=new CreditRecordPO();
 		BeanUtils.copyProperties(creditrecordvo, po);
+		BeanUtils.copyProperties(userService.findByUsername(creditrecordvo.getUsername()).getCredit()+creditrecordvo.getVariation(),userService.findByUsername(creditrecordvo.getUsername()).getCredit());//userService中更新信用值
+		userService.update(userService.findByUsername(creditrecordvo.getUsername()));
 		return creditRecordDAO.add(po);
 	}
 
