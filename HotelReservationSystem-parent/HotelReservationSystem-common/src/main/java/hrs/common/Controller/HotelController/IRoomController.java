fbd3f9903b79124e7aede0a69fa1a2793d5ca354
newsong <songxinjianzx@163.com>
2016-10-27 14:33:58 +0800

    宋欣建 10/27 上传体系结构文档v0.5

--- HotelReservationSystem-parent/HotelReservationSystem-common/src/main/java/hrs/common/Controller/HotelController/IRoomController.java
+++ HotelReservationSystem-parent/HotelReservationSystem-common/src/main/java/hrs/common/Controller/HotelController/IRoomController.java
@@ -7,9 +7,8 @@ import hrs.common.util.ResultMessage;
 import hrs.common.util.type.RoomType;
 
 public interface IRoomController {
-
 	ResultMessage updateRoom(RoomVO roomvo);
 	ResultMessage addRoom(RoomVO roomvo);
 	List<RoomType> findNotAddedRoomType(int hotelID);
-	RoomVO findRoomByHotelAndType(int hotelID,RoomType type);
+	List<RoomVO> findByHotelID(int hotelID);
 }
