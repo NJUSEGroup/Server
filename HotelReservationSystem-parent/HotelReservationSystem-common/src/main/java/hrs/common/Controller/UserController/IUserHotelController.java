006365b2091d5490963de07754dd94b2d2df31ea
newsong <songxinjianzx@163.com>
2016-10-27 20:57:46 +0800

    宋欣建 10/27 修改体系结构文档部分接口

--- HotelReservationSystem-parent/HotelReservationSystem-common/src/main/java/hrs/common/Controller/UserController/IUserHotelController.java
+++ HotelReservationSystem-parent/HotelReservationSystem-common/src/main/java/hrs/common/Controller/UserController/IUserHotelController.java
@@ -12,16 +12,18 @@ import hrs.common.VO.RoomVO;
 import hrs.common.util.type.OrderRule;
 
 public interface IUserHotelController {
-	
+
 	/**
 	 * 前两个方法是为了显示酒店详细信息准备的
+	 * 
 	 * @param hotelID
 	 * @param username
 	 * @return
 	 */
-	List<OrderVO> findOrderByHotelAndUsername(int hotelID,String username);
+	List<OrderVO> findOrderByHotelAndUsername(int hotelID, String username);
+
 	List<RoomVO> getRoomDetail(int hotelID);
-	
+
 	Map<HotelVO, List<OrderVO>> findOrderedHotelAndOrder(String username);
 
 	Map<HotelVO, List<RoomVO>> findHotels(int loc, int circle, Date begin, Date end);
@@ -31,6 +33,7 @@ public interface IUserHotelController {
 	Map<HotelVO, List<RoomVO>> orderHotels(OrderRule rule, boolean isDecrease);
 
 	List<LocationVO> findAllLocations();
+
 	List<CommercialCircleVO> findCircleByLoc(int locID);
-	
+
 }
