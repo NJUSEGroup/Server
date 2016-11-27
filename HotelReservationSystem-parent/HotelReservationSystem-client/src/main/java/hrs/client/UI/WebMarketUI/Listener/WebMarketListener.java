9406ba6be3a08cec70e00e55457bb46bf6ec6e6a
yaozheng1998 <1150681747@qq.com>
2016-11-27 19:07:51 +0800

    11/27 包

---
+++ HotelReservationSystem-parent/HotelReservationSystem-client/src/main/java/hrs/client/UI/WebMarketUI/Listener/WebMarketListener.java
@@ -0,0 +1,5 @@
+package hrs.client.UI.WebMarketUI.Listener;
+
+public class WebMarketListener {
+
+}
---
+++ HotelReservationSystem-parent/HotelReservationSystem-client/src/main/java/hrs/client/UI/WebStaffUI/WebStaffListener/WebStaffListener.java
@@ -0,0 +1,5 @@
+package hrs.client.UI.WebStaffUI.WebStaffListener;
+
+public class WebStaffListener {
+
+}
