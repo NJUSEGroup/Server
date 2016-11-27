8e5825549e23da3086ae1bed027dd353749db24d
yaozheng1998 <1150681747@qq.com>
2016-11-27 21:01:03 +0800

    ee

--- HotelReservationSystem-parent/HotelReservationSystem-common/src/main/java/hrs/common/util/constants.java
+++ HotelReservationSystem-parent/HotelReservationSystem-common/src/main/java/hrs/common/util/constants.java
@@ -6,9 +6,9 @@ import java.awt.Font;
 public class constants {
 	final public static Color jframe=new Color(211,237,249);
 	final public static Color jlabel=new Color(0,160,233);
-	final public static Font jzoneChinese=new Font("Arial Unicode MS", Font.PLAIN, 26);
-	final public static Font jlabelChinese=new Font("Arial Unicode MS", Font.PLAIN, 23);
-	final public static Font English=new Font("Arial Unicode MS", Font.PLAIN, 23);
+	final public static Font jzoneChinese=new Font("Arial Unicode MS", Font.PLAIN, 25);
+	final public static Font jlabelChinese=new Font("Arial Unicode MS", Font.PLAIN, 21);
+	final public static Font English=new Font("Arial Unicode MS", Font.PLAIN, 21);
 	
 
 }
