d4f5c2307951fd6576ac346ed51bda7d1cc375c9
yaozheng1998 <1150681747@qq.com>
2016-11-27 20:49:37 +0800

    11/27 font colors

---
+++ HotelReservationSystem-parent/HotelReservationSystem-common/src/main/java/hrs/common/util/constants.java
@@ -0,0 +1,14 @@
+package hrs.common.util;
+
+import java.awt.Color;
+import java.awt.Font;
+
+public class constants {
+	final public static Color jframe=new Color(211,237,249);
+	final public static Color jzone=new Color(0,160,233);
+	final public static Font jzoneChinese=new Font("Arial Unicode MS", Font.PLAIN, 26);
+	final public static Font jlabelChinese=new Font("Arial Unicode MS", Font.PLAIN, 23);
+	final public static Font English=new Font("Arial Unicode MS", Font.PLAIN, 23);
+	
+
+}
